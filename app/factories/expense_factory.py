e5a3ab0d3438da1c883d4e465bea9e98c4741fd4
MahamTKhan <mahamtkhan2@gmail.com>
2025-03-26 02:52:47 +0500

    Removed Multiton and updated factory logic

--- app/factories/expense_factory.py
+++ app/factories/expense_factory.py
@@ -1,6 +1,34 @@
-from app.models import Expense
-from app.multiton import ExpenseCategoryMultiton
+# from app.models import Expense
+# from app.multiton import ExpenseCategoryMultiton
 
+
+# class ExpenseFactory:
+#     """
+#     Factory pattern for creating new expense instances based on provided attributes.
+#     """
+
+#     @staticmethod
+#     def create_expense(name, amount, category, date, user_id):
+#         """
+#         Factory method for generating a new expense entry.
+
+#         :param name: str - the expense name
+#         :param amount: float - the expense amount
+#         :param category: str - the expense category
+#         :param date: str - the expense date
+#         :param user_id: int - ID of the user who created the expense
+#         :return: Expense instance
+#         """
+#         category_instance = ExpenseCategoryMultiton(category)
+#         return Expense(
+#             name=name,
+#             amount=amount,
+#             category=category_instance.category,
+#             date=date,
+#             user_id=user_id,
+#         )
+
+from app.models import Expense
 
 class ExpenseFactory:
     """
@@ -19,11 +47,10 @@ class ExpenseFactory:
         :param user_id: int - ID of the user who created the expense
         :return: Expense instance
         """
-        category_instance = ExpenseCategoryMultiton(category)
         return Expense(
             name=name,
             amount=amount,
-            category=category_instance.category,
+            category=category,  # Directly use the category string
             date=date,
             user_id=user_id,
         )
